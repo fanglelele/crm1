608f34a6e15134a1b80b388c67786a279c468ed4
fangle <2830979628@qq.com>
2021-12-14 15:10:59 +0800

    crm系统的提交

---
+++ src/main/java/com/yjxxt/crm/Starter.java
@@ -0,0 +1,21 @@
+package com.yjxxt.crm;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+@MapperScan("com.yjxxt.crm.mapper")
+public class Starter extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(Starter.class,args);
+
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return  builder.sources(Starter.class);
+    }
+}
